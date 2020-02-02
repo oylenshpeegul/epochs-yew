b79d74007b1bb127bcee600aa3f8f326e1d32cfa
Tim Heaney <oylenshpeegul@pm.me>
2020-02-02 10:31:25 -0500

    add apfs
    how to avoid recursion limit?

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,45 @@
-#![recursion_limit = "256"]
+#![recursion_limit = "512"]
 
+use chrono::NaiveDateTime;
+use epochs;
 use yew::{html, Component, ComponentLink, Html, InputData, ShouldRender};
 
 pub struct Model {
     link: ComponentLink<Self>,
-    int_value: i64,
-    decimal_str: String,
-    hexadecimal_str: String,
+    int: i64,
+    decimal: String,
+    hexadecimal: String,
+
+    apfs: String,
+    unix: String,
 }
 
 pub enum Msg {
     GotDecimal(String),
     GotHexadecimal(String),
+    GotApfs(String),
+    GotUnix(String),
 }
 
 fn reset(m: &mut Model) {
-    m.int_value = 0;
-    m.decimal_str = "".into();
-    m.hexadecimal_str = "".into();
+    m.int = 0;
+    m.decimal = "".into();
+    m.hexadecimal = "".into();
+
+    m.apfs = "".into();
+    m.unix = "".into();
 }
+
 fn set(m: &mut Model, int: i64) {
-    m.int_value = int;
-    m.decimal_str = format!("{}", int);
-    m.hexadecimal_str = format!("{:x}", int);
+    m.int = int;
+    m.decimal = format!("{}", int);
+    m.hexadecimal = format!("{:x}", int);
+    if let Some(ndt) = epochs::apfs(int) {
+        m.apfs = format!("{}", ndt);
+    }
+    if let Some(ndt) = epochs::unix(int) {
+        m.unix = format!("{}", ndt);
+    }
 }
 
 impl Component for Model {
@@ -32,9 +49,12 @@ impl Component for Model {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Model {
             link,
-            int_value: 0,
-            decimal_str: "".into(),
-            hexadecimal_str: "".into(),
+            int: 0,
+            decimal: "".into(),
+            hexadecimal: "".into(),
+
+            apfs: "".into(),
+            unix: "".into(),
         }
     }
 
@@ -46,12 +66,22 @@ impl Component for Model {
                 } else {
                     reset(self);
                 };
-            },
+            }
             Msg::GotHexadecimal(new_value) => {
                 if let Ok(int) = i64::from_str_radix(&new_value, 16) {
                     set(self, int);
                 } else {
                     reset(self);
+                };
+            }
+            Msg::GotApfs(new_value) => {
+                if let Ok(ndt) = NaiveDateTime::parse_from_str(&new_value, "%Y-%m-%d %H:%M:%S") {
+                    set(self, epochs::to_apfs(ndt));
+                };
+            }
+            Msg::GotUnix(new_value) => {
+                if let Ok(ndt) = NaiveDateTime::parse_from_str(&new_value, "%Y-%m-%d %H:%M:%S") {
+                    set(self, epochs::to_unix(ndt));
                 };
             }
         }
@@ -60,22 +90,43 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div class="grid-container">
-                <div class="grid-item">{"Decimal"}</div>
-                <textarea class="grid-item" rows="1"
-                oninput=self.link.callback(|e: InputData| Msg::GotDecimal(e.value))
-                value={ &self.decimal_str }
-                placeholder="decimal digits">
-                { &self.decimal_str }
-                </textarea>
+            <div>
+                <h1>{"epochs"}</h1>
+                <div class="grid-container">
 
-                <div class="grid-item">{"Hexadecimal"}</div>
-                <textarea class="grid-item" rows="1"
-                oninput=self.link.callback(|e: InputData| Msg::GotHexadecimal(e.value))
-                value={ &self.hexadecimal_str }
-                placeholder="hex digits">
-                { &self.hexadecimal_str }
-                </textarea>
+                    <div class="grid-item">{"Decimal"}</div>
+                    <textarea class="grid-item" rows="1"
+                      oninput=self.link.callback(|e: InputData| Msg::GotDecimal(e.value))
+                      value={ &self.decimal }
+                      placeholder="decimal digits">
+                      { &self.decimal }
+                    </textarea>
+
+                    <div class="grid-item">{"Hexadecimal"}</div>
+                    <textarea class="grid-item" rows="1"
+                      oninput=self.link.callback(|e: InputData| Msg::GotHexadecimal(e.value))
+                      value={ &self.hexadecimal }
+                      placeholder="hex digits">
+                      { &self.hexadecimal }
+                    </textarea>
+
+                    <div class="grid-item">{"APFS"}</div>
+                    <textarea class="grid-item" rows="1"
+                      oninput=self.link.callback(|e: InputData| Msg::GotApfs(e.value))
+                      value={ &self.apfs }
+                      placeholder="yyyy-mm-dd HH:MM:SS">
+                      { &self.apfs }
+                    </textarea>
+
+                    <div class="grid-item">{"Unix"}</div>
+                    <textarea class="grid-item" rows="1"
+                      oninput=self.link.callback(|e: InputData| Msg::GotUnix(e.value))
+                      value={ &self.unix }
+                      placeholder="yyyy-mm-dd HH:MM:SS">
+                      { &self.unix }
+                    </textarea>
+
+                </div>
             </div>
         }
     }
